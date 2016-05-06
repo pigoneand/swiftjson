e3d31d77dbb4dc9fe2767a6542ba7fe8b62221f8
pigoneand <sjtu.pigoneand@gmail.com>
2016-05-06 10:14:20 +0800

    add unittest

---
+++ swiftjson/swiftjson.h
@@ -0,0 +1,19 @@
+//
+//  swiftjson.h
+//  swiftjson
+//
+//  Created by 朱 一和 on 16/5/6.
+//  Copyright © 2016年 pigoneand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftjson.
+FOUNDATION_EXPORT double swiftjsonVersionNumber;
+
+//! Project version string for swiftjson.
+FOUNDATION_EXPORT const unsigned char swiftjsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftjson/PublicHeader.h>
+
+
--- swiftjsonTests/swiftjsonTests.swift
+++ swiftjsonTests/swiftjsonTests.swift
@@ -1,15 +1,17 @@
 //
-//  IsPrimeTest.swift
-//  swiftjson
+//  swiftjsonTests.swift
+//  swiftjsonTests
 //
-//  Created by 朱 一和 on 16/4/28.
+//  Created by 朱 一和 on 16/5/6.
 //  Copyright © 2016年 pigoneand. All rights reserved.
 //
 
 import XCTest
 
-class IsPrimeTest: XCTestCase {
+@testable import swiftjson
 
+class swiftjsonTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,17 +21,18 @@ class IsPrimeTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let (_, offset) = NilTokenGenerator("\n", offset: 0);
+        
+        XCTAssertEqual(offset, 1);
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
